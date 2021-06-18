b8089baad4a4782a24bb0412e428bff1786c6518
Larisa <flafla842@gmail.com>
2021-06-18 16:55:04 +0300

    changed move left

---
+++ 5.1_k_8_Fulfiling_with_spiral.py
@@ -0,0 +1,24 @@
+n,m = map(int,input().split())
+l = [[0 for i in range(m)] for i in range(n)]
+c = 0
+i = 0
+j = -1
+while c != n*m:
+    while j < m - 1 and l[i][j+1] == 0:   # move left
+        j += 1
+        c += 1
+        l[i][j] = c
+    while i < n - 1 and l[i+1][j] == 0:   # двигаюсь вниз
+       i += 1
+       c += 1
+       l[i][j] = c
+    while j > 0 and l[i][j-1] == 0 :   # двигаюсь вправо
+       j -= 1
+       c += 1
+       l [i][j] = c
+    while i > 0 and l[i - 1][j] == 0:   # двигаюсь вверх
+       i -= 1
+       c += 1
+       l[i][j] = c
+for row in l:
+    print(*row)
